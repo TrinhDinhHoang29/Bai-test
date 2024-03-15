d391cf4f32e547c86c9f2a662fbb6e222fe1e5c4
TrinhDinhHoang29 <behoangloduoc@gmail.com>
2024-03-15 20:00:48 +0700

    Đã hoàn thành

--- test.js
+++ test.js
@@ -1,4 +1,25 @@
-let classHoang=document.querySelector("#hoang");
-classHoang.onkeydown=(event)=>{
-console.log(event.key)
-}
+ 
+//  function setASD(){
+//     var opacity=10;
+//         let notification = document.getElementById("notification");
+
+//     let anThongBao=setInterval(()=>{
+//         if(opacity<=0){
+//             clearInterval(anThongBao);
+//             notification.style.display='none';
+//         }
+//         else
+//            { 
+//             notification.style.opacity=opacity;
+//             opacity-=1;
+//         }
+        
+//     },100)
+//  }
+
+
+// // setTimeout(setASD,2000);
+//         let notification = document.getElementById("notification");
+//         notification.innerHTML="Nội dung mới";
+let titles=document.querySelectorAll('.title');
+titles.forEach(element=>element.style.color='blue');
